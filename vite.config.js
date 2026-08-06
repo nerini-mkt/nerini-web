be14197176c6ede9032d3be6e128c99e275db285
nerini-mkt <debora@nerini.ar>
2026-08-06 18:36:39 -0300

    Implement Rollup warning suppression
    Add custom warning handler for Rollup build

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+ 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === 'MODULE_LEVEL_DIRECTIVE') return
+        warn(warning)
+      }
+    }
+  }
 })
